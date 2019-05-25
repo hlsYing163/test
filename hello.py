cb4a6d4b89a6d18b566a81ca3c35e6f945cc6374
hls <hulianshneg@163.com>
2019-05-25 12:17:34 +0800

    hello.py main()

--- hello.py
+++ hello.py
@@ -2,5 +2,8 @@
 # @Author: hu_liansheng
 # @Date:   2019-05-24 16:55:09
 # @Last Modified by:   hu Liansheng
-# @Last Modified time: 2019-05-25 11:53:59
+# @Last Modified time: 2019-05-25 12:16:53
 print('hello world')
+
+if __name__ == '__main__':
+    print("hello")
