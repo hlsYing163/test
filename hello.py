84c4feef669621bed1355f8f3f5e6b971ebcea13
hls <hulianshneg@163.com>
2019-05-25 11:55:49 +0800

    print hello world

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@
 # @Author: hu_liansheng
 # @Date:   2019-05-24 16:55:09
 # @Last Modified by:   hu Liansheng
-# @Last Modified time: 2019-05-24 16:55:09
+# @Last Modified time: 2019-05-25 11:53:59
+print('hello world')
